ddd4bac594dc0f0105cac065d5a555d04fae7a2a
shanmugamkalaimathi <kalai1113413@gmail.com>
2017-07-28 15:24:02 +0530

    Add files via upload

---
+++ Set651.java
@@ -0,0 +1,13 @@
+package hunter;
+import java.util.Scanner;
+public class Set651 {
+	 public static void main(String[] args) {
+		
+		 Scanner s = new Scanner(System.in);
+	        String str1= s.next();
+	        String str2= s.next();
+	        int str3 ;
+	        str3 = Integer.parseInt(str1)*Integer.parseInt(str2);
+	 System.out.print(str3);
+	 }
+}
